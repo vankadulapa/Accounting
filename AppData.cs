e944682971cfaf9535eec15248982f0dddcf0b15
vankadulapa <vankadulapa@gmail.com>
2023-06-15 19:26:31 +0300

    1

---
+++ AppData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.Models
+
+{
+    static public class AppData
+    {
+        static public AccountingEntities db = new AccountingEntities();
+        static public int CurrentAccessLevel = 0;
+    }
+}
--- ChosePage.xaml.cs
+++ ChosePage.xaml.cs
@@ -16,14 +16,13 @@ using System.Windows.Shapes;
 namespace Accounting
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ChosePage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ChosePage : Page
     {
-        public MainWindow()
+        public ChosePage()
         {
             InitializeComponent();
-            MainFrame.Navigate(new LoginPage());
         }
     }
 }
